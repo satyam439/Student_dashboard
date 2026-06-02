6d3e8c09228a560972db66244419ed0008dd5551
Satyam sharma <satyamsharma@Satyams-MacBook-Pro.local>
2026-06-02 10:33:33 +0530

    Remove debug logs

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-console.log(
-  "URL:",
-  process.env.NEXT_PUBLIC_SUPABASE_URL
-);
-
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
