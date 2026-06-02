4214f3dc77112a62600d2c9075ef6056ae978ff6
Satyam sharma <satyamsharma@Satyams-MacBook-Pro.local>
2026-06-02 19:02:53 +0530

    Final submission ready

--- components/HeroTile.tsx
+++ components/HeroTile.tsx
@@ -11,11 +11,11 @@ export default function HeroTile() {
       className="rounded-3xl bg-linear-to-r from-zinc-900 to-zinc-800 p-8 lg:col-span-2 transition-all duration-300 hover:-translate-y-1"
     >
       <h1 className="text-4xl font-bold">
-        Welcome Back, Satyam!
+        Welcome Back, Satyam Sharma!
       </h1>
 
       <p className="mt-3 text-gray-400">
-        7 Day Learning Streak
+        5 Days Learning Streak
       </p>
     </motion.section>
   );
